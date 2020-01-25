46abcf0eb0c5f724771029a7f0636f64ee713b49
nikitasklyarov <nsklyarov@sarex.io>
2020-01-25 17:37:28 +0300

    start commit; added README;

---
+++ README.md
@@ -0,0 +1,3 @@
+a tiny module for build a [Hadamard matrices](https://en.wikipedia.org/wiki/Hadamard_matrix)
+
+
---
+++ index.js
@@ -0,0 +1,53 @@
+function resize(arr, newSize, defaultValue) {
+  return [
+    ...arr,
+    ...Array(Math.max(newSize - arr.length, 0)).fill(defaultValue)
+  ];
+}
+
+function buildSquareMatrix(orderN, list = []) {
+  const resized = resize(list, orderN, 0);
+  for (i = 0; i < orderN; i++) {
+    resized[i] = resize(list, orderN, 0);
+  }
+
+  return resized;
+}
+
+function buildResidueMatrix(orderN, result) {
+  const p = orderN - 1;
+  const residues = resize([], 2 * p, 0);
+
+  for (i = 0; i < p; i++) {
+    residues[i] = -1;
+  }
+  for (i = 1; i <= (p - 1) / 2; i++) {
+    residues[(i * i) % p] = 1;
+  }
+  for (i = p; i < 2 * p; i++) {
+    residues[i] = residues[i - p];
+  }
+
+  return residues;
+}
+
+function buildHadamardMatrix(orderN, result) {
+  const squareMatrix = buildSquareMatrix(orderN, result);
+  const residueMatrix = buildResidueMatrix(orderN, result);
+
+  for (j = 0; j < orderN; j++) {
+    squareMatrix[0][j] = 1;
+  }
+  for (i = 1; i < orderN; i++) {
+    squareMatrix[i][0] = 1;
+  }
+  for (i = 1; i < orderN; i++) {
+    for (j = 1; j < orderN; j++) {
+      squareMatrix[i][j] = residueMatrix[j - i + (orderN - 1)];
+    }
+  }
+
+  return squareMatrix;
+}
+
+module.exports = buildHadamardMatrix;
